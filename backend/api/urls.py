2ffb3e506c5fecfb75daa45d10c1dc30f4d4e600
Rohit <rohitkori897@gmail.com>
2023-06-14 00:33:17 +0530

    crop into parts done

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import PreviewView
-
+from .views import PreviewView, GoogleLensView, CropView
 
 
 urlpatterns = [
     path('preview/', PreviewView.as_view(), name='preview'),
-
+    path('googlelens/', GoogleLensView.as_view(), name='googlelens'),
+    path('crop/', CropView.as_view(), name='crop')
 ]
+
